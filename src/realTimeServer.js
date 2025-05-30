114a41827dd7a1559882fe6008d9066f9549aea0
Liseth-Poma <84851110+Liseth-Poma@users.noreply.github.com>
2025-05-30 16:32:24 -0500

    Add files via upload

---
+++ src/realTimeServer.js
@@ -0,0 +1,12 @@
+module.exports = (httpServer) => {
+    const { Server} = require('socket.io');
+    const io = new Server(httpServer);
+    io.on("connection", (socket) => {
+        socket.on("message", (message) => {
+            io.emit("message", {
+                user: "Julian",
+                message,
+            });
+        });
+    })
+};
